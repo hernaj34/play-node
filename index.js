1395f558a048e700dd11f369304d4b96b29019d2
Jordi Hernandez <jordi.hernandez@roche.com>
2017-11-30 12:08:08 +0100

    Merge master

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ console.log("Hello world!!");
 // Change from Herneli
 // Line created by hernaj34
 
-<<<<<<< HEAD
 // New line
 
 // New change in master
-=======
-// New line 5
->>>>>>> e5ed33758e2f0ec11552315b40b327126f56c707
